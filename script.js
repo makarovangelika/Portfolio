14026d0af0b70fa83dfe6f46f3dc7618f9180f21
Angelika Makarova <esipovaangelika@mail.ru>
2021-11-19 00:02:25 +0300

    Write script for buttons "Read-more" Build and style the section "Skills" for large screens

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function toggleMenu() {
     menu.classList.toggle("opened");
 }
 burgerButton.addEventListener("click", toggleMenu);
+
+let readmoreButton = document.querySelectorAll("[data-target-id]");
+for (element of readmoreButton) {
+    element.addEventListener("click", function (event) {
+        event.target.classList.toggle("opened");
+        let readmore = document.getElementById(event.target.dataset.targetId);
+        readmore.classList.toggle("opened");
+    });
+}
