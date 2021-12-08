e4e8e714051a7affcf99c542ff04362c225bec65
Angelika Makarova <esipovaangelika@mail.ru>
2021-12-09 01:04:23 +0300

    Add smooth scrolling when clicking anchor links

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ for (element of readmoreButton) {
         readmore.classList.toggle("opened");
     });
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
